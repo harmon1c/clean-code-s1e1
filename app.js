8dd30f46a5b1c9bde3aea39d5be9e46959a521bf
harmon1c <80057115+harmon1c@users.noreply.github.com>
2024-12-19 14:17:23 +0200

    fix: update js image selectors, in js and css files according to rule 3.4, in html-and-css.md

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="task__btn-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="task__btn-delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
     listItem.appendChild(checkBox);
